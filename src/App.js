7715999bf0b5fce0f71f66eadd52f31af3520ed4
Subhendu Sethi <cs13b1027@iith.ac.in>
2019-10-22 01:02:18 +0530

    Comming Soon init.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './app-logo.png';
 import './App.css';
+
+import {
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Switch>
+    <Route path="/"> 
+      <DefaultApp />
+    </Route>
+  </Switch>
+    
   );
+}
+
+function DefaultApp() {
+  return <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h4>Coming Soon!</h4>
+    </header>
+</div>;
 }
 
 export default App;

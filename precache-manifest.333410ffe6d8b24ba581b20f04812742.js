568e6c3c6661be8561a6522102654eca3081a199
Subhendu Sethi <cs13b1027@iith.ac.in>
2019-10-22 09:45:25 +0530

    Updates

---
+++ precache-manifest.333410ffe6d8b24ba581b20f04812742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8894ef31e1d8cd22a823e97087c5b553",
+    "url": "/blurger-app/index.html"
+  },
+  {
+    "revision": "83d5850b2df2c311c147",
+    "url": "/blurger-app/static/css/main.c7922e2e.chunk.css"
+  },
+  {
+    "revision": "4c02fcb18ea11a8363aa",
+    "url": "/blurger-app/static/js/2.b996bd7f.chunk.js"
+  },
+  {
+    "revision": "83d5850b2df2c311c147",
+    "url": "/blurger-app/static/js/main.5ebe16fd.chunk.js"
+  },
+  {
+    "revision": "1b9b4fe5fa5d3edc91dd",
+    "url": "/blurger-app/static/js/runtime-main.5be812b8.js"
+  },
+  {
+    "revision": "06c97186a9b496235d5a19f757166ae9",
+    "url": "/blurger-app/static/media/app-logo.06c97186.png"
+  }
+]);
